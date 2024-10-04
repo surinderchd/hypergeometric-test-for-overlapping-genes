1953a93553914afd23c976206969150f38d98d6d
Surinder Pal <54136463+surinderchd@users.noreply.github.com>
2024-10-04 15:14:11 +0200

    Update Hypergeometric test and bionomial test.R

--- Hypergeometric test and bionomial test.R
+++ Hypergeometric test and bionomial test.R
@@ -13,8 +13,8 @@ phyper(156, 314, 445 - 314, 231, lower.tail = FALSE)
 
 
 # Define parameters for the binomial test
-list1 <- 30  # Number of genes regulated exclusively by fIIai
-list2 <- 14   # Number of genes regulated exclusively by fXai
+list1 <- 30  # Number of genes regulated exclusively by list1
+list2 <- 14   # Number of genes regulated exclusively by list2
 total_specific_genes <- list1 + list2  # Total exclusive genes
 
 # Expected proportion (could be 0.5 if expecting equal regulation)
